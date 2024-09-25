23c1440b8041620c19048d4ff5c3b86cb40fc958
GyeongSu Gim <recona97@gmail.com>
2024-09-25 14:08:30 +0900

    Implement Config classes

--- server/config.py
+++ server/config.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass
 
 from sqlalchemy.engine.url import URL
 
@@ -6,26 +7,28 @@ from sqlalchemy.engine.url import URL
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config: 
-  SECRET_KEY = os.getenv("SECRET_KEY")
-  FLASK_ENV=os.getenv("FLASK_ENV")
-  DEBUG = os.getenv("FLASK_DEBUG")
+@dataclass(frozen=True)
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    FLASK_ENV = os.getenv("FLASK_ENV")
+    DEBUG = os.getenv("FLASK_DEBUG")
 
 
-class DevelopmentConfig(Config): 
-  DEBUG = True
+@dataclass(frozen=True)
+class DevelopmentConfig(Config):
+    url_object = URL.create(
+        "postgresql+psycopg2",
+        username=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+    )
 
-  url_object = URL.create(
-    "postgresql+psycopg2",
-    username=os.getenv("DB_USERNAME"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    database=os.getenv("DB_NAME")
-  )
-
-  SQLALCHEMY_DATABASE_URI = url_object
+    SQLALCHEMY_DATABASE_URI = url_object
+    DEBUG = True
 
 
-class ProductionConfig(Config): 
-  FLASK_ENV = "production"
-  DEBUG = False
+@dataclass(frozen=True)
+class ProductionConfig(Config):
+    FLASK_ENV = "production"
+    DEBUG = False
