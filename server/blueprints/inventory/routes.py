11861e33a45f8bf4884593d2da53e76fc52d3a87
GyeongSu Gim <recona97@gmail.com>
2024-10-03 11:12:32 +0900

    Emasculate routes of inventory api

--- server/blueprints/inventory/routes.py
+++ server/blueprints/inventory/routes.py
@@ -1,12 +1,5 @@
-from apifairy import response
 from . import inventory_bp
 from .models import Category
 from .schema import CategorySchema
 
 category_schema = CategorySchema(many=True)
-
-
-@inventory_bp.route("/list", methods=["GET"])
-@response(category_schema)
-def category():
-    return Category.query.all()
